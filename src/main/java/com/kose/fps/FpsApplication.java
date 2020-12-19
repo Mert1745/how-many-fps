ad398e194825b7654c579cadbd7551bc3494eec9
mert1745 <mertkose1745@gmail.com>
2020-12-20 00:47:55 +0300

    first commit

---
+++ src/main/java/com/kose/fps/FpsApplication.java
@@ -0,0 +1,13 @@
+package com.kose.fps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kose/fps/FpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kose.fps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
