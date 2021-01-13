ab4ea319fc72d8bef4ddc427a124e072198a1f24
mert1745 <mertkose1745@gmail.com>
2021-01-13 22:38:53 +0300

    Sorted for specific items

--- src/main/java/com/kose/fps/GameController.java
+++ src/main/java/com/kose/fps/GameController.java
@@ -22,13 +22,37 @@ public class GameController {
 
     @PostMapping(path = "/getGamesByName", produces = MediaType.APPLICATION_JSON_VALUE)
     public FPSResponseBuilder<List<Game>> getGamesByName(@RequestBody Game game) {
-
         List<Game> gameList = repository.findByNameRegex(String.format(".*%s.*", game.getName()));
+
+        sortListByPlatformSize(gameList);
 
         if (gameList.isEmpty()) {
             return FPSResponseBuilder.<List<Game>>getInstance().status(HttpStatus.NOT_FOUND).body(null);
         }
         return FPSResponseBuilder.<List<Game>>getInstance().status(HttpStatus.OK).body(gameList);
+    }
+
+
+    /**
+     * @param gameList changes the specific items (items that have few platforms) index
+     *                 so that they can be in one line at screen
+     */
+    private void sortListByPlatformSize(List<Game> gameList) {
+        int tempIndex = -1;
+        for (int i = 0; i < gameList.size(); i++) {
+            if (gameList.get(i).getPlatform().size() > 3) {
+                continue;
+            }
+
+            if (tempIndex == -1) {
+                tempIndex = i;
+            } else {
+                Game tempGame = gameList.get(i);
+                gameList.remove(i);
+                gameList.add(tempIndex + 1, tempGame);
+                tempIndex = -1;
+            }
+        }
     }
 
     @PostMapping(path = "/addGame", produces = MediaType.ALL_VALUE, consumes = MediaType.ALL_VALUE)
